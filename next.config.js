7ef9950d4e2a19a37762e53c156fbddb232f5e2e
dkcaliskan <dogukaan.caliskan@gmail.com>
2023-11-05 19:52:35 +0300

    Not found error test v5

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
+  /* i18n: {
     locales: ['en-US'],
     defaultLocale: 'en-US',
-  },
+  }, */
 
   images: {
     remotePatterns: [
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
+// API & Core
+import { Suspense } from 'react';
+
+// Components
+import Loader from '@/components/Shared/UiElements/Loader';
+import ProfileSideSkeleton from '@/components/Shared/UiElements/Skeletons/ProfileSideSkeleton';
+import Footer from '@/components/Navigation/Footer';
+import HomeComponents from './_components/Home';
+import Tags from './_components/Tags';
+import ProfileSidebar from '@/components/Shared/ProfileSidebar';
+
 const Home = () => {
   return (
     <>
       <section className='container mx-auto lg:my-3 lg:px-3 font-main pb-3 lg:grid lg:grid-cols-9 lg:gap-3 xl:gap-6'>
-        test
         {/* <section className='lg:col-start-3 lg:col-span-5'>
           <Suspense fallback={<Loader />}>
             <HomeComponents />
           </Suspense>
         </section> */}
+
         {/* <section className='lg:order-first lgMax:hidden lg:col-span-2'>
           <Suspense fallback={<Loader />}>
             <Tags />
           </Suspense>
         </section> */}
+
         {/* <section className='lg:col-span-2 lgMax:hidden'>
           <Suspense fallback={<ProfileSideSkeleton />}>
             <ProfileSidebar />
