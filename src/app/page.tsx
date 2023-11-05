5a4d8ae0426a6d758df4bd44aa10ab689aa86564
dkcaliskan <dogukaan.caliskan@gmail.com>
2023-11-05 19:54:47 +0300

    Not found error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,27 +13,27 @@ const Home = () => {
   return (
     <>
       <section className='container mx-auto lg:my-3 lg:px-3 font-main pb-3 lg:grid lg:grid-cols-9 lg:gap-3 xl:gap-6'>
-        {/* <section className='lg:col-start-3 lg:col-span-5'>
+        <section className='lg:col-start-3 lg:col-span-5'>
           <Suspense fallback={<Loader />}>
             <HomeComponents />
           </Suspense>
-        </section> */}
+        </section>
 
-        {/* <section className='lg:order-first lgMax:hidden lg:col-span-2'>
+        <section className='lg:order-first lgMax:hidden lg:col-span-2'>
           <Suspense fallback={<Loader />}>
             <Tags />
           </Suspense>
-        </section> */}
+        </section>
 
-        {/* <section className='lg:col-span-2 lgMax:hidden'>
+        <section className='lg:col-span-2 lgMax:hidden'>
           <Suspense fallback={<ProfileSideSkeleton />}>
             <ProfileSidebar />
           </Suspense>
-        </section> */}
+        </section>
       </section>
-      {/* <section>
+      <section>
         <Footer />
-      </section> */}
+      </section>
     </>
   );
 };
